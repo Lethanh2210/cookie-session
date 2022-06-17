f05b0486bf48f94f9ffe82e7dd54e337bbe43885
Your Name <kzxlmdtn888@gmail.com>
2022-06-17 08:41:43 +0700

    ..

--- auto-login-by-session/app.js
+++ auto-login-by-session/app.js
@@ -37,68 +37,60 @@ let createRandomString = function (strLength){
 }
 
 let readSession = function(req, res){
-
 //lấy sessionId từ local storage
-
     let tokenID = localStorage.get("token");
     if (tokenID){
         let sessionString= "";
         let expires=0;
-
-//đọc file sessionId tương ứng phía server
-
+        //đọc file sessionId tương ứng phía server
         fs.readFile('./token/'+tokenID, 'utf8' , (err, data) => {
             if (err) {
                 console.error(err)
                 return
             }
             sessionString = String(data);
-
-// lấy ra thời gian hết hạn của sessionId
-
+            // lấy ra thời gian hết hạn của sessionId
             expires = JSON.parse(sessionString).expires;
-
-// lấy ra thời gian hiện tại
-
+            // lấy ra thời gian hiện tại
             let now = Date.now();
-
-// so sánh thời gian hết hạn với thời hạn của sessionID
-
+            // so sánh thời gian hết hạn với thời hạn của sessionID
             if (expires<now){
-
-//Đã đăng nhập nhưng hết hạn
-
-//Thực hành đăng nhập và lưu lại
-
+                //Đã đăng nhập nhưng hết hạn
+                //xoa session
+                deleteTokenSession(tokenID);
+                //Thực hành đăng nhập và lưu lại
+                router.login(req, res);
+            }
+            else {
+                // Đã đăng nhập và chưa hết hạn
+                // chuyển sang trang dashboard
                 let parseUrl = url.parse(req.url, true);
                 let path = parseUrl.pathname;
                 let trimPath = path.replace(/^\/+|\/+$/g, '');
-                let chosenHandler = (typeof (router[trimPath]) !== 'undefined') ? router[trimPath] : handlers.notfound;
-                chosenHandler(req, res);
-            }
-            else {
+                //Nếu đường dẫn là /logout thì thực hiện xoá session và chuyển về trang login
+                if (trimPath==="logout") {
+                    console.log("logout");
+                    deleteTokenSession(tokenID);
+                    router.login(req, res);
+                }
+                else {
+                    fs.readFile('./views/dashboard.html', 'utf8', function (err, datahtml) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        datahtml = datahtml.replace('{name}', JSON.parse(sessionString).name);
+                        datahtml = datahtml.replace('{email}', JSON.parse(sessionString).email);
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.write(datahtml);
+                        return res.end();
+                    });
+                }
 
-// Đã đăng nhập và chưa hết hạn
-
-// chuyển sang trang dashboard
-
-                fs.readFile('./views/dashboard.html', 'utf8', function (err, datahtml) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    datahtml = datahtml.replace('{name}', JSON.parse(sessionString).name);
-                    datahtml = datahtml.replace('{email}', JSON.parse(sessionString).email);
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(datahtml);
-                    return res.end();
-                });
             }
         });
     }
     else {
-
-// chưa đăng nhập
-
+        // chưa đăng nhập
         let parseUrl = url.parse(req.url, true);
         let path = parseUrl.pathname;
         let trimPath = path.replace(/^\/+|\/+$/g, '');
@@ -106,6 +98,16 @@ let readSession = function(req, res){
         chosenHandler(req, res);
     }
 }
+
+
+let deleteTokenSession = function (fileName){
+    fileName = './token/' + fileName;
+    fs.unlink(fileName, err => {
+        if (err) throw err;
+        console.log('File deleted!');
+    });
+}
+
 
 let handlers = {};
 
